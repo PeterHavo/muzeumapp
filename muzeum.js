f6b30e9dda31872127a948b1df01dc14f6f43f4e
peter.haviar <peter.haviar@accenture.com>
2017-06-05 14:13:08 +0200

    language mutation

--- muzeum.js
+++ muzeum.js
@@ -6,21 +6,37 @@ init();
                         anchors: ['firstPage', 'secondPage', '3rdPage', '4thpage', 'lastPage'],
                          menu: '#menu',
                     });
-                }
-                        var  title = "Kempelen speaking machine";
-                        var content = `<div class="section">${title}
-                                                <div class="jumbotron">
-                                                <h1 class="display-3">${title}</h1>
-                                                <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-                                                <hr class="my-4">
-                                                <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-                                                <p class="lead">
-                                                    <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
-                                                </p>
-                                                </div>
-                                         </div>`
+                    
+                    $('#eng').hide();
 
                  
+                }
+                        // var  title = "Kempelen speaking machine";
+                        // var content = `<div class="section">${title}
+                        //                         <div class="jumbotron">
+                        //                         <h1 class="display-3">${title}</h1>
+                        //                         <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+                        //                         <hr class="my-4">
+                        //                         <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+                        //                         <p class="lead">
+                        //                             <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+                        //                         </p>
+                        //                         </div>
+                        //                  </div>`
+
+                   // slovak eng traslation 
+                   $('button').click(()=>{
+                        
+                          $('#eng').toggle();})
+
+
+                    $('#sktest').click(()=>{
+                      
+                        $('#skcontent').show()})
+
+                      init();
+
+
                         // //adding a section dynamically
                         // $('button').click(function(){
                         //     $('#fullpage').append(content);
